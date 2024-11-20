98ef5c0fd0ec1f2d8781247b4a8d7ce704b3e806
Celiants <130403501+Celiants@users.noreply.github.com>
2024-11-20 14:27:49 +0100

    Résolution de l'issue #2 : Remplacement des '*' par 'x'

--- Q5.java
+++ Q5.java
@@ -3,7 +3,7 @@ public class Q5 {
         int hauteur = Integer.parseInt(args[0]);
         for (int i = hauteur; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print("x");
             }
             System.out.println();
         }
