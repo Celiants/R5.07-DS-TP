409ae7575a0fb552a582be2a32da2f993fb3acf6
Celiants <130403501+Celiants@users.noreply.github.com>
2024-11-20 14:58:56 +0100

    Update Q6.java

--- Q6.java
+++ Q6.java
@@ -3,7 +3,7 @@ public class Q6 {
         int hauteur = Integer.parseInt(args[0]);
         for (int i = hauteur; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("j'ai mit le workflow 5 en disabled car sinon une erreur m'empéchait que le fichier difference.txt fonctionne");
+                System.out.print("xXx");
             }
             System.out.println();
         }
