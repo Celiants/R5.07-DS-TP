650b7bdc961bd794e9a2864f5369f4b65b13f73c
Celiants <130403501+Celiants@users.noreply.github.com>
2024-11-20 14:01:30 +0100

    Create Q3.java

---
+++ Q3.java
@@ -0,0 +1,10 @@
+public class Q3 {
+    public static void main(String[] args)         
+        int nombreEtoiles;
+        
+        for (int i = 0; i < nombreEtoiles; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
