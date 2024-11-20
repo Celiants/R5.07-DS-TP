f76505d6bf760ba6ecbbb4baf6cbe15e1eef3394
Celiants <130403501+Celiants@users.noreply.github.com>
2024-11-20 14:04:14 +0100

    Update Q3.java

--- Q3.java
+++ Q3.java
@@ -1,7 +1,6 @@
 public class Q3 {
-    public static void main(String[] args)         
-        int nombreEtoiles;
-        
+    public static void main(String[] args) {
+        int nombreEtoiles = Integer.parseInt(args[0]);
         for (int i = 0; i < nombreEtoiles; i++) {
             System.out.print("*");
         }
